ba082fa5b48fa55e5c29ba8ba79d57d8c64cef1a
sabinegleizupa07 <97937401+sabinegleizupa07@users.noreply.github.com>
2023-03-28 13:27:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const input1 = document.querySelector('.js-teksts');
-const plusButton = document.querySelector('.js-plus');
+const plusButton = document.querySelector('.js-plus')
 
+const Add = (skaitlis1, skaitlis2) => {
+    return skaitlis1 + skaitlis2
+}
 
-console.log(input1)
-console.log(plusButton)
+const Subtract = (skaitlis1, skaitlis2) => {
+    return skaitlis1 - skaitlis2
+}
+
+const Multiply = (skaitlis1, skaitlis2) => {
+    return skaitlis1 * skaitlis2
+}
+
+const Divide = (skaitlis1, skaitlis2) => {
+    return skaitlis1 / skaitlis2
+}
+
+plusButton.addEventListener('click', () => {
+    console.log("Mani nospieda")
+})
